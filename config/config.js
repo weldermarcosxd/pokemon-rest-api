4074b404b5774237c5eeab80efc3bff9aa3cdb7d
weldermarcosxd <weldermarcosxd@gmail.com>
2016-09-21 02:04:31 -0300

    version 1

--- config/config.js
+++ config/config.js
@@ -1,14 +1,5 @@
 export default {
-        database: 'book',
-        username: '',
-        password: '',
-        params: {
-            dialect: 'mongodb',
-            storage: `${process.env.NODE_ENV}_books.sqlite`,
-            define: {
-                underscore: true
-            }
-        },
+        database: process.env.NODE_ENV,
         jwtSecret: 'P@ss',
         jwtSession: { session: false }
 };
